a3e23ed077ae44ab91624ef0ee063a43ac6609a1
victorlim12 <85238316+victorlim12@users.noreply.github.com>
2022-10-15 16:34:36 +0800

    tidying up

--- src/Pages/Individual/Feedback.js
+++ src/Pages/Individual/Feedback.js
@@ -5,6 +5,8 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+
+
 const colors = {
     orange:"#FFBA5A",
     grey:"#a9a9a9"
